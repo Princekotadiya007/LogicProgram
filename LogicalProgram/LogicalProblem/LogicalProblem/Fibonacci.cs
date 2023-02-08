593fc735e0cc27514e543c1246ca7bec8ea6e110
Princekotadiya007 <princuuu77@gmail.com>
2023-02-08 23:56:57 +0530

    [prince] fibonacciSeries

---
+++ LogicalProgram/LogicalProblem/LogicalProblem/Fibonacci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+    public class Fibonacci
+    {
+        public void fibonacciSeries()
+        {
+            Console.WriteLine("Enter the number of terms in the series: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int first = 0, second = 1, next;
+
+            Console.WriteLine("The first {0} terms of the Fibonacci series are: ", n);
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i <= 1)
+                {
+                    next = i;
+                }
+                else
+                {
+                    next = first + second;
+                    first = second;
+                    second = next;
+                }
+                Console.WriteLine(next);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicalProgram/LogicalProblem/LogicalProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 Fibonacci");
+            Console.WriteLine("choose the option");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.fibonacciSeries();
+                    break;
+                default:
+                    Console.WriteLine("choose the option");
+                    break;
+            }
+        }
+    }
+}
